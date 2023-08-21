6b253e147486f399cdd35475a4f75ae47d15599c
William Diebolt <wmdiebolt@gmail.com>
2023-08-21 11:14:36 -0700

    permute function implemented by calling rand_select from extract_random_elements.ml

--- generate_random_ordered_list.ml
+++ generate_random_ordered_list.ml
@@ -1,17 +1,7 @@
-(* Generate random permutation of the elements in a list *)
+#use "extract_random_elements.ml"
 
-(* Intuition:
- 	getRandom returns a random list element
-	aux recurs until ls = [] and passes getRandom::accumulator which is ret'd on base case. 
+let permute ls = rand_select ls (ls |> List.length)
 
-	Do I find List.length once and keep a count? getRandom has to be supplied with length info somehow. 
-
-	ls |> List.length |> Random.int	
-*)
-
-let permute ls =
-	let starlen = ls |> List.length 
-	let getRandom ls acc 
  
 
 
